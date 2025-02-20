97a7a60adce1cef2462dfae6d926e01dbe010ab7
Mohammadmehdi-web <webauctech110@gmail.com>
2025-02-20 11:02:04 +0530

    convert php

---
+++ db_con.php
@@ -0,0 +1,13 @@
+<?php
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = "weailth_builder_db";
+
+    $con = new mysqli($servername, $username, $password, $database);
+
+    if($con->connect_error){
+        die("connection failed : " .$con->connect_error);
+    }
+?>

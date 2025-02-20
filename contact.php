13a3c6362bbe36fc23a5e092ce7366cf6032ffe8
Mohammadmehdi-web <webauctech110@gmail.com>
2025-02-20 11:12:39 +0530

    include header and footer in contact page

--- contact.php
+++ contact.php
@@ -49,9 +49,9 @@
     <!-- Preloader End  -->
     <!-- Header Start -->
     <header id="header">
-        <script>
-            $('#header').load('header.html');
-        </script>
+        <?php
+            include('header.php');
+        ?>
     </header>
     <!-- Header End -->
     <!-- Off Canvas Start  -->
@@ -192,7 +192,9 @@
     <!-- Contact Page End -->
     <!-- Footer Start -->
     <footer id="footer">
-        <script>$('#footer').load('footer.html') </script>
+        <?php
+            include('footer.php');
+        ?>
     </footer>
     <!-- Scroll Up Section Start -->
     <div id="scrollTop" class="scrollup-wrapper">

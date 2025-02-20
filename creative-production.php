ea9be9b3bf7649ad0f87765f5ac4a9363923d0d9
Mohammadmehdi-web <webauctech110@gmail.com>
2025-02-20 11:14:27 +0530

    include header and footer in creative-production page

--- creative-production.php
+++ creative-production.php
@@ -60,9 +60,9 @@
     <!-- Preloader End  -->
     <!-- Header Start -->
     <header id="header">
-        <script>
-            $('#header').load('header.html');
-        </script>
+        <?php
+            include('header.php');
+        ?>
     </header>
     <!-- Header End -->
     <!-- Off Canvas Start  -->
@@ -399,7 +399,9 @@
     <!-- Services Details Page End -->
     <!-- Footer Start -->
     <footer id="footer">
-        <script>$('#footer').load('footer.html') </script>
+        <?php
+            include('footer.php');
+        ?>
     </footer>
     <!-- Scroll Up Section Start -->
     <div id="scrollTop" class="scrollup-wrapper">
